ea1017df76594142b6e934e6a61f82f3609fc922
armandgray <armandgray@gmail.com>
2018-02-14 16:47:39 -0800

    re-adding handling for displaying suggestions

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ import {
   ListView
 } from 'react-native';
 
-const MAX_EXPECTED_AUTO_CORRECT_DIFFERENCE = 10;
-
 export class MentionsTextInput extends Component {
   constructor(props) {
     super(props);
@@ -66,9 +64,23 @@ export class MentionsTextInput extends Component {
     }).start();
   }
 
-  updateSuggestions(lastKeyword) {
-    this.props.triggerCallback(lastKeyword);
-  }
+  handleDisplaySuggestions(position) {
+      if (!this.triggerMatrix || !this.triggerMatrix.length || !this.isTrackingStarted || this.shouldDeleteTriggerOnBackspace) {
+        return;
+      }
+
+      const keyword = this.getTriggerKeyword(position);
+      const delay = this.props.triggerDelay ? this.props.triggerDelay : 0;
+      if (keyword && keyword.length > delay) {
+        if (this.props.triggerCallback) {
+          this.props.triggerCallback(keyword);
+        }
+
+        this.openSuggestionsPanel();
+      } else {
+        this.closeSuggestionsPanel();
+      }
+    }
 
   handleDeleteTriggerOnBackspace(position = 0, index = -2) {
     if (!this.triggerMatrix || !this.triggerMatrix.length) {
@@ -105,20 +117,59 @@ export class MentionsTextInput extends Component {
     }
   }
 
-  identifyKeyword(val) {
-    if (this.isTrackingStarted) {
-      const boundary = this.props.triggerLocation === 'new-word-only' ? 'B': '';
-      const delay = this.props.triggerDelay ? this.props.triggerDelay : 0;
-      const pattern = new RegExp(`\\${boundary}${this.props.trigger}[a-z0-9_-]{${delay},}`, `gi`);
-      const keywordArray = val.match(pattern);
-      if (keywordArray && !!keywordArray.length) {
-        const lastKeyword = keywordArray[keywordArray.length - 1];
-        this.updateSuggestions(lastKeyword);
-        this.openSuggestionsPanel();
-      } else {
-        this.closeSuggestionsPanel();
-      }
+  handleTriggerSplitBySpace(position) {
+    if (!this.triggerMatrix
+          || !this.triggerMatrix.length
+          || !this.isTrackingStarted
+          || position < 1
+          || position >= this.state.text.length) {
+      return;
     }
+
+    const index = this.getSubsequentTriggerIndex(position);
+    this.triggerMatrix[index][1] = position - 1;
+  }
+
+  isTriggerSplitBySpace(position) {
+    if (!this.triggerMatrix || !this.triggerMatrix.length) {
+      return false;
+    }
+
+    const index = this.getSubsequentTriggerIndex(position);
+    return this.isPositionWithinTrigger(position, index)
+              && this.isTrackingStarted
+              && this.state.text[position] === ' ';
+  }
+
+  getDistanceToNextSpace(index = -1) {
+    if (index === -1 || !this.state.text || !this.state.text.length || index > this.state.text.length) {
+      return 0;
+    }
+
+    const spaceIndex = this.state.text.indexOf(' ', index);
+    return spaceIndex === -1 ? this.state.text.length - 1 - index : spaceIndex - index;
+  }
+
+  getTriggerKeyword(position, index = -2) {
+    if (!this.triggerMatrix || !this.triggerMatrix.length || !this.isTrackingStarted) {
+      return;
+    }
+
+    if (index === -2) {
+      index = this.getSubsequentTriggerIndex(position);
+    }
+
+    if (index === -1 || index >= this.triggerMatrix.length) {
+      return;
+    }
+
+    const start = this.triggerMatrix[index][0];
+    const end = this.triggerMatrix[index][1];
+    const pattern = new RegExp(`${this.props.trigger}[a-z0-9_-]*`, `gi`);
+    const triggerText = this.state.text.slice(start, end + this.getDistanceToNextSpace(end) + 1);
+    const keywordArray = triggerText.match(pattern);
+
+    return keywordArray && !!keywordArray.length ? keywordArray[0] : '';
   }
 
   updateTriggerMatrixIndex(position) {
@@ -126,19 +177,13 @@ export class MentionsTextInput extends Component {
       return;
     }
 
-    const index = this.triggerMatrixIndex;
-    if (index === -1 || index >= this.triggerMatrix.length) {
+    const index = this.getSubsequentTriggerIndex(position);
+    const keyword = this.getTriggerKeyword(position, index);
+    if (!keyword || !keyword.length) {
       return;
     }
 
-    const pattern = new RegExp(`${this.props.trigger}[a-z0-9_-]*`, `gi`);
-    const triggerText = this.state.text.slice(this.triggerMatrix[index][0], this.triggerMatrix[index][1] + MAX_EXPECTED_AUTO_CORRECT_DIFFERENCE);
-    const keywordArray = triggerText.match(pattern);
-
-    if (keywordArray && !!keywordArray.length) {
-      const keyword = keywordArray[0];
-      this.triggerMatrix[index][1] = this.triggerMatrix[index][0] + keyword.length - 1;
-    }
+    this.triggerMatrix[index][1] = this.triggerMatrix[index][0] + keyword.length - 1;
   }
 
   stopTracking() {
@@ -201,18 +246,12 @@ export class MentionsTextInput extends Component {
 
     if (this.isTriggerMatrixEmpty(index)) {
       this.triggerMatrix = [[position, position]];
-      this.triggerMatrixIndex = 0;
 
     } else if (this.isPositionBeforeNextTrigger(position, index)) {
       this.triggerMatrix.splice(index, 0, [position, position]);
-      this.triggerMatrixIndex = index;
 
     } else if (this.isPositionAfterBiggestTrigger(position, index)) {
       this.triggerMatrix.push([position, position]);
-      this.triggerMatrixIndex = this.triggerMatrix.length - 1;
-
-    } else if (this.isPositionWithinTrigger(position, index)) {
-      this.triggerMatrixIndex = index;
     }
   }
 
@@ -327,6 +366,10 @@ export class MentionsTextInput extends Component {
     } else if (!this.isTrackingStarted && lastChar === this.props.trigger && wordBoundary) {
       this.startTracking(position);
 
+    } else if (this.isTriggerSplitBySpace(position)) {
+      this.handleTriggerSplitBySpace(position);
+      this.stopTracking();
+
     } else if (this.isTrackingStarted && (lastChar === ' ' || this.state.text === '')) {
       this.stopTracking();
 
@@ -334,21 +377,24 @@ export class MentionsTextInput extends Component {
       this.updateTriggerMatrixIndex(position);
     }
 
-    this.identifyKeyword(this.state.text);
+    this.handleDisplaySuggestions(this.state.text);
   }
 
   onSelectionChange(selection) {
     this.setState({ selection });
 
+    const position = selection.end - 1;
     if (this.didTextChange && selection.start === selection.end) {
-      this.handleTyping(selection.start - 1);
+      this.handleTyping(position);
 
     } else if (selection.start === selection.end) {
-      this.handleClick(selection.start - 1);
+      this.handleClick(position);
 
     } else {
       // cursor selecting chars from selection.start to selection.end
     }
+
+    this.handleDisplaySuggestions(position);
 
     this.didTextChange = false;
     this.isTriggerDeleted = false;
